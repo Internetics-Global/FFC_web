59fd2b7a4f66ce8485a291373313030ce219ab60
Clive France <clive@internetics.net.au>
2021-08-15 03:22:06 +0000

    First commit final

---
+++ app/Views/auth_internetics/footer.php
@@ -0,0 +1,24 @@
+
+			</div>
+			
+		</div>
+		
+	</main>
+
+
+
+
+        <script src="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/js/jquery-1.11.1.min.js"></script>
+        <script src="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/js/jquery-plugins/jquery.form.js"></script>
+        <script src="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/js/common/cache-library.js"></script>
+        <script src="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/js/common/common.js"></script>
+        <script src="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/js/jquery-plugins/gc-dropdown.min.js"></script>
+        <script src="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/js/jquery-plugins/gc-modal.min.js"></script>
+        <script src="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/js/jquery-plugins/bootstrap-growl.min.js"></script>
+        <script src="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/js/jquery-plugins/jquery.print-this.js"></script>
+        <script src="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/js/datagrid/gcrud.datagrid.js"></script>
+        <script src="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/js/datagrid/list.js"></script>
+        <script src="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/js/lightbox.js"></script>
+</body>
+</html>
+
---
+++ app/Views/auth_internetics/header.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+	<meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php echo $title; ?></title>
+
+	<link type="text/css" rel="stylesheet" href="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/css/bootstrap/bootstrap.min.css"/>
+    <link type="text/css" rel="stylesheet" href="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/css/elusive-icons/css/elusive-icons.min.css"/>
+    <link type="text/css" rel="stylesheet" href="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/css/common.css"/>
+    <link type="text/css" rel="stylesheet" href="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/css/list.css"/>
+    <link type="text/css" rel="stylesheet" href="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/css/general.css"/>
+    <link type="text/css" rel="stylesheet" href="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/css/plugins/animate.min.css"/>
+    <link type="text/css" rel="stylesheet" href="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/css/main.css"/>
+    <link type="text/css" rel="stylesheet" href="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/css/internetics.css?v=2"/>
+    <link type="text/css" rel="stylesheet" href="https://www.internetics.net.au/ffcweb/public/assets/grocery_crud/themes/internetics/css/lightbox.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+    
+    
+    
+
+
+</head>
+<body>
+<nav class="navbar navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand mx-auto" href="#">Flip Flash Cards Web Edition</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="/ffcweb/public/index.php/product/screening_management">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="add">Add card</a>
+      </li>
+    </ul>
+
+  </div>
+</nav>
+
+<main class="bd-content p-5" role="main">
+	
+	 <div class="row">
+		 
+      	<div class="col-12">
